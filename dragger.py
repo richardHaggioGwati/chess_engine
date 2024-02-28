7f2d0787e42febb036048f7b4ad89cd585dcecb0
richardHaggioGwati <haggiog0844@gmail.com>
2024-02-28 22:42:03 +0200

    (feat): complete_two_player_mode

--- dragger.py
+++ dragger.py
@@ -2,7 +2,6 @@ import pygame
 
 from const import *
 
-
 class Dragger:
 
     def __init__(self):
@@ -13,25 +12,33 @@ class Dragger:
         self.initial_row = 0
         self.initial_col = 0
 
-    def update_mouse(self, position):
-        self.mouseX, self.mouseY = position
+    # blit method
 
-    def save_initial(self, position):
-        self.initial_row = position[1] // SQSIZE
-        self.initial_col = position[0] // SQSIZE
+    def update_blit(self, surface):
+        # texture
+        self.piece.set_texture(size=128)
+        texture = self.piece.texture
+        # img
+        img = pygame.image.load(texture)
+        # rect
+        img_center = (self.mouseX, self.mouseY)
+        self.piece.texture_rect = img.get_rect(center=img_center)
+        # blit
+        surface.blit(img, self.piece.texture_rect)
+
+    # other methods
+
+    def update_mouse(self, pos):
+        self.mouseX, self.mouseY = pos # (xcor, ycor)
+
+    def save_initial(self, pos):
+        self.initial_row = pos[1] // SQSIZE
+        self.initial_col = pos[0] // SQSIZE
 
     def drag_piece(self, piece):
         self.piece = piece
         self.dragging = True
 
-    def stop_dragging_piece(self):
+    def undrag_piece(self):
         self.piece = None
         self.dragging = False
-
-    def update_blit(self, surface):
-        self.piece.set_texture(size=128)
-        texture = self.piece.texture
-        img = pygame.image.load(texture)
-        img_center = (self.mouseX, self.mouseY)
-        self.piece.texture_rect = img.get_rect(center=img_center)
-        surface.blit(img, self.piece.texture_rect)
